3e151fca3cb4ac46460b3c48c6c67a599bb72e36
Ryan J Dobrynski <ryjedo@gmail.com>
2014-09-24 16:58:34 -0600

    lvl-b6 complete with logic map

--- ryjedo-beginner/player.rb
+++ ryjedo-beginner/player.rb
@@ -2,32 +2,70 @@ class Player
 
 	def initialize
 		@health = 20
+		@enough_health = 10
+		@found_level_start = false
 	end
 
 	def play_turn(warrior)
-		if @health > warrior.health #im being attacked
-			if warrior.health > 10 #i have enough health to continue
-				advance_and_attack(warrior)
-			else #i need to retreat
+		if @health > warrior.health #i'm being attacked
+			if warrior.health > @enough_health #i have enough health to press the attack
+				advance_forward(warrior)
+			else #i'm low on health and must retreat
 				warrior.walk!(:backward)
 			end
-		else #im not being attacked
-			if warrior.health < 20
+		else #i'm not being attacked
+			if warrior.health < 20 #I'm not full health
 				warrior.rest!
-			else
-				advance_and_attack(warrior)
+			else #i am full health
+				if @found_level_start == false #i havent found the start of the level and should look for it.
+					advance_backward(warrior)
+				else #i've found the start of the level and can continue forward.
+					advance_forward(warrior)
+				end	
 			end
-		end
+ 		end
 		@health = warrior.health
 	end
 
-	def advance_and_attack(warrior)
-		if warrior.feel.enemy?
-			warrior.attack!
-		elsif warrior.feel.captive?
-			warrior.rescue!
-		elsif warrior.feel.empty?
-			warrior.walk!
+	def advance_forward(warrior)
+		direction = :forward
+		case detect_space_type(warrior.feel(direction))
+		when :empty || :stairs
+			warrior.walk!(direction)
+		when :enemy
+			warrior.attack!(direction)
+		when :captive
+			warrior.rescue!(direction)
+		end
+	end
+
+	def advance_backward(warrior)
+		direction = :backward
+		case detect_space_type(warrior.feel(direction))
+		when :empty 
+			warrior.walk!(direction)
+		when :enemy
+			warrior.attack!(direction)
+		when :captive
+			warrior.rescue!(direction)
+		when :wall
+			@found_level_start = true
+		end
+	end
+
+	def detect_space_type(space)
+		if space.empty?
+			return :empty
+		elsif space.enemy?
+			return :enemy
+		elsif space.captive?
+			return :captive
+		elsif space.wall?
+			return :wall
+		elsif space.stairs?
+			return :stairs
+		else
+			return :unkown_space
 		end
 	end
 
