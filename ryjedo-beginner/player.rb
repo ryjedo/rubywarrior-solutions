e745bc54ce89218df8b5a95887adf3e5368db6fa
Ryan J Dobrynski <ryjedo@gmail.com>
2014-09-23 18:44:42 -0600

    lvl-b4 complete

--- ryjedo-beginner/player.rb
+++ ryjedo-beginner/player.rb
@@ -1,13 +1,30 @@
 class Player
-  def play_turn(warrior)
-    if warrior.feel.empty?
-    	if warrior.health < 10
-    		warrior.rest!
-    	else
-    		warrior.walk!
-    	end
-    else
-    	warrior.attack!
-    end
-  end
+
+
+	def initialize
+		@health = 20
+	end
+
+	def play_turn(warrior)
+		if @health > warrior.health
+			#im being attacked
+			advance_and_attack(warrior)
+		else
+			#im not being attacked
+			if warrior.health < 20
+				warrior.rest!
+			else
+				advance_and_attack(warrior)
+			end
+		end
+		@health = warrior.health
+	end
+
+	def advance_and_attack(warrior)
+		if warrior.feel.enemy?
+			warrior.attack!
+		else
+			warrior.walk!
+		end
+	end
 end
