d7ad51561c440331a52ff3040479f56f73937a43
Ryan J Dobrynski <ryjedo@gmail.com>
2014-09-30 14:52:42 -0600

    solves lvl-i2

--- odejyr-intermediate/player.rb
+++ odejyr-intermediate/player.rb
@@ -1,5 +1,38 @@
 class Player
-  def play_turn(warrior)
-    warrior.walk!(warrior.direction_of_stairs)
-  end
+
+	def play_turn(warrior)
+    
+		if locate_enemy(warrior) == :empty
+			if warrior.health == 20
+				warrior.walk!(warrior.direction_of_stairs)
+			else
+				warrior.rest!
+			end
+		else
+			warrior.attack!(locate_enemy(warrior))
+		end
+	end
+
+
+	def locate_enemy(warrior)
+
+		return_value = :nil
+
+		if warrior.feel(:forward).enemy?
+			return_value = :forward
+		elsif warrior.feel(:left).enemy?
+			return_value = :left
+		elsif warrior.feel(:right).enemy?
+			return_value = :right
+		elsif warrior.feel(:backward).enemy?
+			return_value = :backward
+		else
+			return_value = :empty
+		end	
+		
+		return return_value
+
+	end
+
+
 end
